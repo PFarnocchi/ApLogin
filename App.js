059f9e15631cdd8147e16c08a47bf5152ea995f0
PFarnocchi <pfarnocchi@gmail.com>
2022-01-27 13:21:00 -0300

    ApLogin

--- App.js
+++ App.js
@@ -1,12 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Button, Input, Text } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function App() {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const entrar = () => {
+    console.log("Entrou no ApLogin")
+    console.log("eMail = ", email)
+    console.log("Senha = ", password)
+  }
+ 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text h2>Entrar no ApLogin !</Text>
+      <Input
+          placeholder='E-mail'
+          leftIcon={{ type: 'font-awesome', name: 'envelope'}}
+          onChangeText={value => setEmail(value)}
+          keyboardType='email-address'
+      />
+      <Input
+          placeholder='Senha'
+          leftIcon={{ type: 'font-awesome', name: 'lock'}}
+          onChangeText={value => setPassword(value)}
+          secureTextEntry={true}
+      />
+      <Button
+          icon={
+            <Icon
+                name="check"
+                size={15}
+                color="white"
+            />
+          }
+          title="Manda bala"
+          onPress={() => entrar()}
+      />    
     </View>
   );
 }
